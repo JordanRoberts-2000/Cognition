a6df7185b1ce0f15ea50b7b8531780fb8a017591
Jordan <jordanroberts333@icloud.com>
2023-10-17 21:26:36 +0100

    ui improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Category from "./components/categories/Category"
 import Header from "./components/ui/header/Header"
+import data from "./data/data.json"
 
 const App = () => {
     return (
         <>
             <Header />
             <div className="flex flex-col md:flex-row">
-                <div className="flex-[2]">
+                <div className="flex-[2] sticky top-11 bg-white border-b-2 border-black">
                     <ul className="flex md:flex-col gap-4 md:gap-1 w-full overflow-x-auto p-2">
                         <li>All</li>
                         <li>Bookmarked</li>
@@ -20,7 +21,7 @@ const App = () => {
                     </ul>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-5 w-full gap-2 flex-[8] pb-2 px-2 md:pt-2">
-                    {[...Array(30)].map((i) => <Category key={i} />)}
+                    {data.allData.map((el, i) => <Category key={i} title={el.name}/>)}
                 </div>
             </div>
         </>
--- src/components/categories/Category.tsx
+++ src/components/categories/Category.tsx
@@ -1,15 +1,23 @@
+type Props = {
+    title: string
+}
 
-
-const Category = () => {
+const Category = ({title}: Props) => {
     return (
-        <div className="aspect-square flex flex-col border border-slate-400">
-            <div className="flex-1"></div>
+        <div className="aspect-video md:aspect-square flex flex-col border border-slate-400">
+            <div className="flex-1 flex items-center justify-center text-5xl font-bold">
+                {title}
+            </div>
             <div className="border-slate-200 border p-2 flex">
-                <svg className="w-6 h-6 stroke-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+                <div className="flex">
+                    <svg className="w-6 h-6 stroke-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+                    </svg>
+                    <svg className="h-5 fill-green-200 ml-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32v96V384c0 35.3 28.7 64 64 64H256V384H64V160H256V96H64V32zM288 192c0 17.7 14.3 32 32 32H544c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32H445.3c-8.5 0-16.6-3.4-22.6-9.4L409.4 9.4c-6-6-14.1-9.4-22.6-9.4H320c-17.7 0-32 14.3-32 32V192zm0 288c0 17.7 14.3 32 32 32H544c17.7 0 32-14.3 32-32V352c0-17.7-14.3-32-32-32H445.3c-8.5 0-16.6-3.4-22.6-9.4l-13.3-13.3c-6-6-14.1-9.4-22.6-9.4H320c-17.7 0-32 14.3-32 32V480z" /></svg>
+                </div>
+                <svg className="w-6 h-6 ml-auto stroke-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                 </svg>
-
-                <svg className="h-5 fill-green-200 ml-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32v96V384c0 35.3 28.7 64 64 64H256V384H64V160H256V96H64V32zM288 192c0 17.7 14.3 32 32 32H544c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32H445.3c-8.5 0-16.6-3.4-22.6-9.4L409.4 9.4c-6-6-14.1-9.4-22.6-9.4H320c-17.7 0-32 14.3-32 32V192zm0 288c0 17.7 14.3 32 32 32H544c17.7 0 32-14.3 32-32V352c0-17.7-14.3-32-32-32H445.3c-8.5 0-16.6-3.4-22.6-9.4l-13.3-13.3c-6-6-14.1-9.4-22.6-9.4H320c-17.7 0-32 14.3-32 32V480z" /></svg>
             </div>
         </div>
     )
