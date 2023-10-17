40e89b19809d7a00aeb75fa202119614d3e274bd
Jordan <jordanroberts333@icloud.com>
2023-10-17 23:32:26 +0100

    more ui improvements

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
         <>
             <Header />
             <div className="flex flex-col md:flex-row">
-                <div className="flex-[2] sticky top-11 bg-white border-b-2 border-black">
+                <div className="flex-[2] sticky top-11 bg-white border-b-2 border-black z-10">
                     <ul className="flex md:flex-col gap-4 md:gap-1 w-full overflow-x-auto p-2">
                         <li>All</li>
                         <li>Bookmarked</li>
@@ -20,7 +20,7 @@ const App = () => {
                         <li>Languages</li>
                     </ul>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-5 w-full gap-2 flex-[8] pb-2 px-2 md:pt-2">
+                <div className="grid grid-cols-1 md:grid-cols-5 w-full gap-4 flex-[8] p-4">
                     {data.allData.map((el, i) => <Category key={i} title={el.name}/>)}
                 </div>
             </div>
