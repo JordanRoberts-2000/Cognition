7a77ca3cd886b04bb21533100f32d6f0d62772cc
Jordan <jordanroberts333@icloud.com>
2023-10-18 00:04:30 +0100

    even more ui improvements

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,18 @@ const App = () => {
         <>
             <Header />
             <div className="flex flex-col md:flex-row">
-                <div className="flex-[2] sticky top-11 bg-white border-b-2 border-black z-10">
-                    <ul className="flex md:flex-col gap-4 md:gap-1 w-full overflow-x-auto p-2">
-                        <li>All</li>
-                        <li>Bookmarked</li>
-                        <li>Priority</li>
-                        <li>Databases</li>
-                        <li>Backend</li>
-                        <li>Frontend</li>
-                        <li>OS</li>
-                        <li>Cloud</li>
-                        <li>Languages</li>
+                <div className="flex-[2] sticky top-11 bg-white border-b border-slate-300 z-10">
+                    <ul className="flex md:flex-col gap-4 md:gap-1 w-full overflow-x-auto p-2 font-medium">
+                        <li className="border-2 border-black px-2 rounded-md">All</li>
+                        <li className="border-2 border-black px-2 rounded-md">Bookmarked</li>
+                        <li className="border-2 border-black px-2 rounded-md">Priority</li>
+                        <li className="border-2 border-black px-2 rounded-md">Databases</li>
+                        <li className="border-2 border-black px-2 rounded-md">Backend</li>
+                        <li className="border-2 border-black px-2 rounded-md">Frontend</li>
+                        <li className="border-2 border-black px-2 rounded-md">OS</li>
+                        <li className="border-2 border-black px-2 rounded-md">Cloud</li>
+                        <li className="border-2 border-black px-2 rounded-md">Languages</li>
+                        <li className="px-2 rounded-md bg-black text-white">+</li>
                     </ul>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-5 w-full gap-4 flex-[8] p-4">
--- src/components/categories/Category.tsx
+++ src/components/categories/Category.tsx
@@ -6,24 +6,26 @@ const Category = ({ title }: Props) => {
     return (
         <div className="aspect-video md:aspect-square flex flex-col rounded-md customShadow">
             <div className="flex-1 relative flex items-center justify-center font-bold">
-                <span className="text-5xl gradientText">{title}</span>
-                <div className="flex gap-1 absolute bottom-2 left-2 text-sm">
+                <span className="text-5xl gradientText py-2 mb-2">{title}</span>
+                <div className="flex gap-2 absolute bottom-2 left-2 text-sm">
                     <div className="bg-blue-50 px-2 py-[1px] text-blue-400 font-semibold">Difficultly</div>
                     <div className="bg-blue-50 px-2 py-[1px] text-blue-400 font-semibold">Type</div>
                 </div>
             </div>
-            <div className="border-slate-200 border p-2 flex">
-                <div className="flex">
-                    <svg className="w-6 h-6 stroke-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}>
+            <div className="border-slate-200 border p-2 flex items-center">
+                <div className="flex gap-4 border border-slate-300 rounded-md px-2 p-1">
+                    <svg className="w-6 h-6 stroke-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
                     </svg>
-                    <svg className="h-5 fill-green-200 ml-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32v96V384c0 35.3 28.7 64 64 64H256V384H64V160H256V96H64V32zM288 192c0 17.7 14.3 32 32 32H544c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32H445.3c-8.5 0-16.6-3.4-22.6-9.4L409.4 9.4c-6-6-14.1-9.4-22.6-9.4H320c-17.7 0-32 14.3-32 32V192zm0 288c0 17.7 14.3 32 32 32H544c17.7 0 32-14.3 32-32V352c0-17.7-14.3-32-32-32H445.3c-8.5 0-16.6-3.4-22.6-9.4l-13.3-13.3c-6-6-14.1-9.4-22.6-9.4H320c-17.7 0-32 14.3-32 32V480z" /></svg>
+                    <svg className="w-6 h-6 stroke-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    </svg>
                 </div>
-                <div className="ml-auto flex gap-2">
-                    <svg className="w-6 h-6 stroke-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}>
+                <div className="ml-auto flex gap-6">
+                    <svg className="w-5 h-5 stroke-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                     </svg>
-                    <svg className="w-6 h-6 stroke-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}>
+                    <svg className="w-5 h-5 stroke-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
